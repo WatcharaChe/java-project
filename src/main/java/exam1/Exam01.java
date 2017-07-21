354a41aa277ef5579f83e115b6c413726a5eb06b
watcharaChe <watchara.che@gmail.com>
2017-07-21 15:34:26 +0700

    Test 22 failed

--- src/main/java/exam1/Exam01.java
+++ src/main/java/exam1/Exam01.java
@@ -1,7 +1,9 @@
 package exam1;
 
 public class Exam01 {
-    private int socrePlayerA = 0;
+    private int scorePlayerA = 0;
+    private int scorePlayerB = 0;
+    private String whoGetPoint = "";
 
     public Exam01(String firstPerson, String secondPerson) {
     }
@@ -9,33 +11,51 @@ public class Exam01 {
 
     public void playerAWin() {
         int point = 15;
-        if (socrePlayerA >= 30) {
+        if (scorePlayerA >= 30) {
             point = 10;
         }
-        socrePlayerA += point;
+        scorePlayerA += point;
+        whoGetPoint = "A";
     }
 
     public void playerBWin() {
+        int point = 15;
+        if (scorePlayerB >= 30) {
+            point = 10;
+        }
+        scorePlayerB += point;
+        whoGetPoint = "B";
     }
 
     public String getScore() {
         String score = "";
-        switch (socrePlayerA) {
-            case 15:
-                score = "Fifteen-Love";
-                break;
-            case 30:
-                score = "Thirty-Love";
-                break;
-            case 40:
-                score = "Forty-Love";
-                break;
-            case 50:
-                score = "Win for Player A";
-                break;
-            default:
-                score = "Love-All";
+
+        if (whoGetPoint.equals("A")) {
+            switch (scorePlayerA) {
+                case 15:
+                    score = "Fifteen-Love";
+                    break;
+                case 30:
+                    score = "Thirty-Love";
+                    break;
+                case 40:
+                    score = "Forty-Love";
+                    break;
+                case 50:
+                    score = "Win for Player A";
+                    break;
+            }
+        } else if (whoGetPoint.equals("B")) {
+            switch (scorePlayerB) {
+                case 15:
+                    score = "Love-Fifteen";
+                    break;
+            }
+        } else {
+            score = "Love-All";
         }
+
+
         return score;
     }
 
