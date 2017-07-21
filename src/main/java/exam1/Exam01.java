97a5cfc2ef33faf28b5e26904b1f9ff363de869e
watcharaChe <watchara.che@gmail.com>
2017-07-21 15:25:24 +0700

    Test 1-4 Pass

--- src/main/java/exam1/Exam01.java
+++ src/main/java/exam1/Exam01.java
@@ -1,14 +1,18 @@
 package exam1;
 
 public class Exam01 {
-    private int playerA = 0;
+    private int socrePlayerA = 0;
 
     public Exam01(String firstPerson, String secondPerson) {
     }
 
 
     public void playerAWin() {
-        playerA += 15;
+        int point = 15;
+        if (socrePlayerA == 30) {
+            point = 10;
+        }
+        socrePlayerA += point;
     }
 
     public void playerBWin() {
@@ -16,12 +20,15 @@ public class Exam01 {
 
     public String getScore() {
         String score = "";
-        switch (playerA) {
+        switch (socrePlayerA) {
             case 15:
                 score = "Fifteen-Love";
                 break;
             case 30:
                 score = "Thirty-Love";
+                break;
+            case 40:
+                score = "Forty-Love";
                 break;
             default:
                 score = "Love-All";
