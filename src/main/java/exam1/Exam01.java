cfdba7bec6a22047e38a959997b42eb94a6430f9
watcharaChe <watchara.che@gmail.com>
2017-07-21 16:54:59 +0700

    All 31 tests passed Clean Code

--- src/main/java/exam1/Exam01.java
+++ src/main/java/exam1/Exam01.java
@@ -38,24 +38,6 @@ public class Exam01 {
         whoGetPoint = "A";
     }
 
-    public void playerAWinTEMP() {
-        int point = 15;
-        if (scorePlayerA == 45) {
-            point = 5;
-        } else if (scorePlayerB == 45 && scorePlayerA == 40) {
-            scorePlayerB -= 5;
-            whoGetPoint = "";
-            return;
-        } else if (scorePlayerA == 40 && scorePlayerB == 40) {
-            point = 5;
-        } else if (scorePlayerA >= 30) {
-            point = 10;
-        }
-
-        scorePlayerA += point;
-        whoGetPoint = "A";
-    }
-
     public void playerBWin() {
         int point = 15;
         if (deuce.equals("B")) {
@@ -79,94 +61,6 @@ public class Exam01 {
         whoGetPoint = "B";
     }
 
-    public void playerBWinTEMP() {
-        int point = 15;
-        if (scorePlayerB == 45) {
-            point = 5;
-        } else if (scorePlayerA == 45 && scorePlayerB == 40) {
-            scorePlayerA -= 5;
-            whoGetPoint = "";
-            return;
-        } else if (scorePlayerA == 40 && scorePlayerB == 40) {
-            point = 5;
-        } else if (scorePlayerB >= 30) {
-            point = 10;
-        }
-        scorePlayerB += point;
-        whoGetPoint = "B";
-    }
-
-    public String getScoreTEMP() {
-        String score = "";
-
-        if (scorePlayerA == 50) {
-            score = "Win for Player A";
-        } else if (scorePlayerB == 50) {
-            score = "Win for Player B";
-        } else if (scorePlayerA == 45) {
-            score = "Advantage Player A";
-        } else if (scorePlayerB == 45) {
-            score = "Advantage Player B";
-        } else if (scorePlayerA == scorePlayerB) {
-            switch (scorePlayerA) {
-                case 15:
-                    score = "Fifteen-All";
-                    break;
-                case 30:
-                    score = "Thirty-All";
-                    break;
-                case 40:
-                    score = "Deuce";
-                    break;
-                case 50:
-                    score = "Win for Player A";
-                    break;
-                case 0:
-                    score = "Love-All";
-                    break;
-            }
-
-            return score;
-        } else {
-            score = convertNumber2String(scorePlayerA) + "-" + convertNumber2String(scorePlayerB);
-        }
-
-
-        if (whoGetPoint.equals("A") && scorePlayerB == 0) {
-            switch (scorePlayerA) {
-                case 15:
-                    score = "Fifteen-Love";
-                    break;
-                case 30:
-                    score = "Thirty-Love";
-                    break;
-                case 40:
-                    score = "Forty-Love";
-                    break;
-                case 50:
-                    score = "Win for Player A";
-                    break;
-            }
-        } else if (whoGetPoint.equals("B") && scorePlayerA == 0) {
-            switch (scorePlayerB) {
-                case 15:
-                    score = "Love-Fifteen";
-                    break;
-                case 30:
-                    score = "Love-Thirty";
-                    break;
-                case 40:
-                    score = "Love-Forty";
-                    break;
-                case 50:
-                    score = "Win for Player B";
-                    break;
-            }
-        }
-
-
-        return score;
-    }
 
     public String getScore() {
         String score = "";
